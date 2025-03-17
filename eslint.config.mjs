8deb29296f45f96e527f02b1597106d00cdc4b45
JorgeDev <jorgehumbertocolindres@gmail.com>
2025-03-16 20:42:35 -0600

    fix: Update ESLint configuration to include no-unused-vars rule and improve formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,16 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
-
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^req|res|next$" }]
+    }
+  }
 ];
