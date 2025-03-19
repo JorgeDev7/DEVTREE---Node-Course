47be5ade494ac573a44409d5c7e0b21252738ae4
JorgeDev <jorgehumbertocolindres@gmail.com>
2025-03-19 11:46:53 -0600

    feat: Add MongoDB connection setup and integrate with server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
+import "dotenv/config";
 import e from "express";
+import { connectDB } from "./config/db";
 import { router } from "./router";
 
 const app = e();
-
+connectDB();
 // Leer datos de formularios (JSON)
 app.use(e.json());
 
