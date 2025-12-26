9d3c6fc91940b49015a18a2119d47d47ef026ed0
JorgeDev <jorgehumbertocolindres@gmail.com>
2025-12-26 11:06:51 -0600

    feat: add cors config and allowed origins

---
+++ src/config/cors.ts
@@ -0,0 +1,12 @@
+import { CorsOptions } from "cors";
+
+export const corsConfig: CorsOptions = {
+  origin: function (origin, callback) {
+    const allowedOrigins = [process.env.FRONTEND_URI];
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
+import cors from "cors";
 import "dotenv/config";
 import e from "express";
+import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
 import { router } from "./router";
 
-const app = e();
 connectDB();
+
+const app = e();
+
+//Cors
+app.use(cors(corsConfig));
 // Leer datos de formularios (JSON)
 app.use(e.json());
 
