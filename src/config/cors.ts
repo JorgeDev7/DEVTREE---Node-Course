c55342e219e10983d7f26de682f44a68cfb6f31f
JorgeDev <jorgehumbertocolindres@gmail.com>
2025-12-27 23:20:34 -0600

    config: allow external dev domains

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,6 +3,11 @@ import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const allowedOrigins = [process.env.FRONTEND_URI];
+
+    if (process.argv[2] === "--api") {
+      allowedOrigins.push(undefined);
+    }
+
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
